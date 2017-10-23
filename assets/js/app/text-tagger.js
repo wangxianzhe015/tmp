642ddc724c41ffabc8ebb6308deaef4877faf205
wangxianzhe015 <xianzhe_wang@yahoo.com>
2017-10-23 10:52:32 +0800

    tagger update

--- assets/js/app/text-tagger.js
+++ assets/js/app/text-tagger.js
@@ -66,7 +66,7 @@
                     }
                 }
             }
-        }).appendTo("."+taggerContainerClassName);
+        }).appendTo("."+taggerContainerClassName).hide();
 
         $("<img/>",{
             class: "tagger-icon tagger-icon-unhightlight",
@@ -83,7 +83,7 @@
         }).on("click", function(){
             if (window.getSelection().toString() == "")return false;
             $("#tagger-keyword-div").show();
-        }).appendTo("."+taggerContainerClassName);
+        }).appendTo("."+taggerContainerClassName).hide();
 
         $("<div></div>", {
             id: "tagger-keyword-div"
@@ -91,7 +91,32 @@
 
         $("<select></select>", {
             id: "tagger-keyword-select"
+        }).on("change", function(){
+            var keyword = $(this).val();
+            if (keyword == "") return false;
+            $(this).parent().hide();
+            if (typeof window.getSelection != "undefined") {
+                var sel = window.getSelection(), $obj = $(sel.focusNode.parentNode);
+                $obj.attr("data-keyword", keyword);
+                $("#" + $obj.attr("id") + "-keyword").html(keyword);
+            }
+
+            if (window.getSelection) {
+                if (window.getSelection().empty) {  // Chrome
+                    window.getSelection().empty();
+                } else if (window.getSelection().removeAllRanges) {  // Firefox
+                    window.getSelection().removeAllRanges();
+                }
+            } else if (document.selection) {  // IE?
+                document.selection.empty();
+            }
+
         }).appendTo("#tagger-keyword-div");
+
+        $("<option></option>", {
+            value: "",
+            text: "-- Select keyword --"
+        }).appendTo("#tagger-keyword-select");
 
         keywords.forEach(function(word){
             $("<option></option>", {
@@ -100,103 +125,36 @@
             }).appendTo("#tagger-keyword-select");
         });
 
-        $("<button></button>", {
-            id: "tagger-keyword-set-btn",
-            class: "btn",
-            text: "Set"
-        }).on("click", function(){
+        $("<span></span>", {
+            id: "tagger-unhighlight-btn",
+            text: "X"
+        }).on("click", function(e){
+            e.preventDefault();
+            e.stopPropagation();
             $("#tagger-keyword-div").hide();
+            var sel = window.getSelection(), data = sel.toString(), $obj = $(sel.focusNode.parentNode), $container = $obj.parents(".tagger-container");
+            if (data != "") {
+                $("#" + $obj.attr("id") + "-keyword").remove();
+                $obj.replaceWith(data);
+                $container.html($container.html());
 
-            var keyword = $("#tagger-keyword-select").val();
-            if (typeof window.getSelection != "undefined") {
-                var sel = window.getSelection(), range, data, children, highlightTag, keywordTag, isSecond = false;
-                if (sel.rangeCount) {
-                    for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-                        range = sel.getRangeAt(i);
-                        data = range.cloneContents();
-                        children = $(data).children();
-                        children.each(function(i,el){
-                            console.log($(el));
-                            if ($(el).parents(".second").length > 0){
-                                isSecond = true;
-                            }
-                        });
-                        if (isSecond) return false;
-                        range.deleteContents();
-                        var randomId = "highlighted-word-" + parseInt(Math.random() * 1000000000);
-                        highlightTag = $("<code></code>", {
-                            id: randomId,
-                            class: "tagger-highlight-text",
-                            "data-keyword": keyword,
-                            html: data
-                        }).on({
-                            click: function(){
-                                $("#"+$(this).attr("id")+"-keyword").addClass("selected");
-                            },
-                            mouseover: function(){
-                                var $obj = $("#"+$(this).attr("id")+"-keyword");
-                                if (!$obj.hasClass("selected")) {
-                                    $obj.show();
-                                }
-                            },
-                            mouseleave: function(){
-                                var $obj = $("#"+$(this).attr("id")+"-keyword");
-                                if (!$obj.hasClass("selected")) {
-                                    $obj.hide();
-                                }
-                            }
-                        });
-                        keywordTag = $("<p></p>",{
-                            id: randomId + "-keyword",
-                            class: "tagger-highlight-keyword",
-                            "data-target": randomId,
-                            text: keyword
-                        }).on({
-                            dblclick: function(){
-                                $("#"+$(this).attr("data-target")).attr("data-keyword","");
-                                $(this).remove();
-                            }
-                        });
-
-                        if (children.length > 0){
-                            $(keywordTag).appendTo($(this).parents(".tagger-icons-container").next().find(".tagger-keyword-right-panel"));
-                            highlightTag.addClass("second");
-                            //range.insertNode(document.createElement("br"));
-                            range.insertNode(highlightTag.get(0));
-                        } else {
-                            $(keywordTag).appendTo($(this).parents(".tagger-icons-container").next().find(".tagger-keyword-left-panel"));
-                            highlightTag.addClass("first");
-                            range.insertNode(highlightTag.get(0));
-                            //range.insertNode(document.createElement("br"));
-                        }
-
-                        if (window.getSelection) {
-                            if (window.getSelection().empty) {  // Chrome
-                                window.getSelection().empty();
-                            } else if (window.getSelection().removeAllRanges) {  // Firefox
-                                window.getSelection().removeAllRanges();
-                            }
-                        } else if (document.selection) {  // IE?
-                            document.selection.empty();
-                        }
+                if (window.getSelection) {
+                    if (window.getSelection().empty) {  // Chrome
+                        window.getSelection().empty();
+                    } else if (window.getSelection().removeAllRanges) {  // Firefox
+                        window.getSelection().removeAllRanges();
                     }
+                } else if (document.selection) {  // IE?
+                    document.selection.empty();
                 }
             }
 
         }).appendTo("#tagger-keyword-div");
 
-        $("<button></button>", {
-            text: "Cancel",
-            class: "btn"
-        }).on("click", function(){
-            $("#tagger-keyword-div").hide();
-        }).appendTo("#tagger-keyword-div");
     }
 
     function unhighlightAll(target){
         var $obj = $("#" + target);
-        $obj.find(".tagger-highlight-save-icon").remove();
-        $obj.find("br").remove();
         var hiddens = $obj.find(".textillate-out-span");
         hiddens.each(function(i,el){
             $(el).replaceWith($(el).find(".texts").text());
@@ -244,13 +202,83 @@
             }
             //TODO: save app
             console.log("App Save");
+            $(".tagger-icon-cut").show();
         })).insertAfter(playground);
 
         if ($(this).find(".tagger-icons-container").length == 0){
             addTaggerIcons($(this).attr("id"));
         }
         return this.on("mouseup", function(event) {
-            if (window.getSelection().toString() == "")return false;
+            if (typeof window.getSelection != "undefined") {
+                var sel = window.getSelection(), range, data, children, highlightTag, keywordTag, isSecond = false;
+                if (sel.toString() == "")return false;
+                if (sel.rangeCount) {
+                    for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                        range = sel.getRangeAt(i);
+                        data = range.cloneContents();
+                        children = $(data).children();
+                        children.each(function(i,el){
+                            if ($(el).parents(".second").length > 0){
+                                isSecond = true;
+                            }
+                        });
+                        if (isSecond) return false;
+                        range.deleteContents();
+                        var randomId = "highlighted-word-" + parseInt(Math.random() * 1000000000);
+                        highlightTag = $("<code></code>", {
+                            id: randomId,
+                            class: "tagger-highlight-text",
+                            "data-keyword": "",
+                            html: data
+                        }).on({
+                            click: function(){
+                                $("#"+$(this).attr("id")+"-keyword").addClass("selected");
+                            },
+                            mouseover: function(){
+                                var $obj = $("#"+$(this).attr("id")+"-keyword");
+                                if ($obj.html() == "") return false;
+                                if (!$obj.hasClass("selected")) {
+                                    $obj.show();
+                                }
+                            },
+                            mouseleave: function(){
+                                var $obj = $("#"+$(this).attr("id")+"-keyword");
+                                if (!$obj.hasClass("selected")) {
+                                    $obj.hide();
+                                }
+                            }
+                        });
+                        keywordTag = $("<p></p>",{
+                            id: randomId + "-keyword",
+                            class: "tagger-highlight-keyword",
+                            "data-target": randomId,
+                            text: ""
+                        }).on({
+                            dblclick: function(){
+                                $("#"+$(this).attr("data-target")).attr("data-keyword","");
+                                $(this).remove();
+                            }
+                        });
+
+                        if (children.length > 0){
+                            $(keywordTag).appendTo($(this).parents(".playground").find(".tagger-keyword-right-panel"));
+                            highlightTag.addClass("second");
+                            //range.insertNode(document.createElement("br"));
+                            range.insertNode(highlightTag.get(0));
+                        } else {
+                            $(keywordTag).appendTo($(this).parents(".playground").find(".tagger-keyword-left-panel"));
+                            highlightTag.addClass("first");
+                            range.insertNode(highlightTag.get(0));
+                            //range.insertNode(document.createElement("br"));
+                        }
+                        sel.addRange(range);
+                    }
+                }
+                $("#tagger-keyword-div").css({
+                    left: event.originalEvent.clientX,
+                    top: event.originalEvent.clientY + 10
+                }).show();
+            }
         }).on("mousedown", function(){
         }).on("paste", function(e){
             var clipboardData = (e.originalEvent || e).clipboardData.getData("text/plain");
