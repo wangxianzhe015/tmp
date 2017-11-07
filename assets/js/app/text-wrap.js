d6136950a613b7912ee059ea96ea6f84fea91d72
wangxianzhe015 <xianzhe_wang@yahoo.com>
2017-11-07 22:39:59 +0800

    multiple wrap

--- assets/js/app/text-wrap.js
+++ assets/js/app/text-wrap.js
@@ -1,19 +1,28 @@
 function boundary(object, array){
-    var $object;
-    $(object).each(function(index,obj){
+    var $object = $(object),isMultiple = false,classPrefix="",height;
+    if ($object.find(".boundary-layout").length > 0){
+        $object = $(object).find(".boundary-layout");
+        isMultiple = true;
+    }
+    $object.each(function(index,obj){
         $object = $(obj);
-        if ($object.hasClass("boundary-layout")){
-            $object.find(".left-boundary").html(array[0]);
-            $object.find(".top-boundary").html(array[1]);
-            $object.find(".right-boundary").html(array[2]);
-            $object.find(".bottom-boundary").html(array[3]);
-            return true;
-        }
-        //var defaultWidth = "100%", defaultHeight = "100%";
+        //if ($object.hasClass("boundary-layout")){
+        //    $object.find(".left-boundary").html(array[0]);
+        //    $object.find(".top-boundary").html(array[1]);
+        //    $object.find(".right-boundary").html(array[2]);
+        //    $object.find(".bottom-boundary").html(array[3]);
+        //    return true;
+        //}
+
         var $newObject = $("<div></div>", {
             class: "boundary-layout",
             id: "boundary-layout-" + parseInt(Math.random() * 100000000000)
         });
+
+        if (isMultiple){
+            $newObject.addClass("boundary boundary-layout-content");
+            classPrefix = "inner-";
+        }
 
         var words = "Jun-07 EUGDRP" +
         "07 JETBLUE<br/>" +
@@ -23,26 +32,32 @@ function boundary(object, array){
         "12 SCTI CLAIM<br/>" +
         "13 DEXE-PHERIN<br/>" +
         "15";
-        $newObject.html("<div class='boundary boundary-layout-content'>" + words + "</div>");
-        $newObject.prependTo($object);
+        $newObject.html("<div class='boundary " + classPrefix+ "boundary-layout-content'>" + words + "</div>");
+        if (isMultiple){
+            $object.find(".boundary-layout-content").replaceWith($newObject);
+            height = $newObject.innerHeight();
+        } else {
+            $newObject.prependTo($object);
+            height = $object.innerHeight();
+        }
 
         $("<div></div>", {
-            class: "boundary left-boundary",
+            class: "boundary " + classPrefix+ "left-boundary",
             text: array[0]
-        }).css("width", $object.innerHeight() - 3 * parseInt($object.css("font-size")) - 20).prependTo($newObject);
+        }).css("width", height - 3 * parseInt($object.css("font-size")) - 20).prependTo($newObject);
 
         $("<div></div>", {
-            class: "boundary top-boundary",
+            class: "boundary " + classPrefix+ "top-boundary",
             text: array[1]
         }).prependTo($newObject);
 
         $("<div></div>", {
-            class: "boundary right-boundary",
+            class: "boundary " + classPrefix+ "right-boundary",
             text: array[2]
-        }).css("width", $object.innerHeight() - 3 * parseInt($object.css("font-size")) - 20).appendTo($newObject);
+        }).css("width", height - 3 * parseInt($object.css("font-size")) - 20).appendTo($newObject);
 
         $("<div></div>", {
-            class: "boundary bottom-boundary",
+            class: "boundary " + classPrefix+ "bottom-boundary",
             text: array[3]
         }).appendTo($newObject);
 
