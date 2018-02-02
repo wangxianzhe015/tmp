d4cc72e3c491d570a44a67f6d73265635ab8f9e0
wangxianzhe015 <xianzhe_wang@yahoo.com>
2018-02-02 15:31:48 +0700

    calendar data keep

---
+++ assets/js/app/cookie.js
@@ -0,0 +1,20 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue) {
+    var expires = "expires=01 Jan 2070 00:00:00 UTC;";
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
--- assets/js/app/text-wrap.js
+++ assets/js/app/text-wrap.js
@@ -1,11 +1,24 @@
 var enableHide = true;
 
-function boundary(object, array){
-    var $object = $(object),isMultiple = false,classPrefix="",height;
+function boundary(object){
+    var $object = $(object), $obj,isMultiple = false,classPrefix="",height, array, cookieExist = true, words, tmp;
+    if (getCookie("wrapper-words") == ""){
+        cookieExist = false;
+    }
     if ($object.find(".boundary-layout").length > 0){
         $object = $(object).find(".boundary-layout");
         isMultiple = true;
+        if (cookieExist){
+            array = $.parseJSON(getCookie("wrapper-words"))["outer"];
+        } else {
+            array = {"left": "SUPREME COURT:::,WITNESS PREP:::,BARRISTER:::,FORENSIC:::,GIA EXAM:::", "top": "ALI:::,ROWLING:::,DYLAN GORDON:::,YURY SHAR:::", "right": "STUART:::,IAN MOORE:::,VALERI:::,TAMMY:::,IRINA:::,JASON:::,IP ASSIGNME:::", "bottom": "44 Bay St:::,EIN Delaware:::,LLC UK Ltd:::,ASIC 404:::"};
+        }
     } else {
+        if (cookieExist){
+            array = $.parseJSON(getCookie("wrapper-words"))["inner"];
+        } else {
+            array = {"left": "Reuters:::,Bloomberg:::,Yummmy Car:::,Medellin:::,Freelancer:::,Bing:::,HR Policy:::,Delegates Sheet:::,Sophin:::,TFN:::,APEC:::", "top": "SAP:::,ADOBE:::,IBM:::,REED:::,MICROSOFT:::,GOOGLE:::,ORACLE:::,ATLASSIAN:::,SALESFORCE:::,DROPBOX:::,31:::","right": "Kayak Trip Advisor:::,Amoma:::,Yandex:::,Bing:::,Google:::,Intertrust:::,VISA:::,James Stavrou:::,Jason Vieges:::,T & C Love:::", "bottom": "..cn:::,.de:::,.ru:::,.com:::,.ko:::,.co.r:::,.uk:::,redvat.com:::,detectasum magnify:::"};
+        }
         $("body").append($("<div></div>", {
             class: "tagger-tooltip",
             id: "image-tooltip"
@@ -23,7 +36,9 @@ function boundary(object, array){
             id: "tooltip-text",
             class: "form-control no-margin no-padding"
         }).on("keyup", function(){
-            $(".active-party").html($(this).val());
+            var $obj = $("." + $(".active-party")[0].className.split(" ")[1]);
+            $obj.html($(this).val());
+            updateCookieWords();
         })).append($("<h3></h3>", {
             class: "white"
         })).append($("<div></div>", {
@@ -34,12 +49,16 @@ function boundary(object, array){
             class: "form-control width-half",
             placeholder: "Event Date"
         }).on("change", function(){
-            $(".active-party").data("date", $(this).val());
+            var $obj = $("." + $(".active-party")[0].className.split(" ")[1]);
+            $obj.data("date", $(this).val());
+            updateCookieWords();
         }).datepicker()).append($("<select></select>", {
             id: "tagger-message-select",
             class: "form-control width-half"
         }).on("change", function(){
-            $(".active-party").data("importance", $(this).val());
+            var $obj = $("." + $(".active-party")[0].className.split(" ")[1]);
+            $obj.data("importance", $(this).val());
+            updateCookieWords();
         }))).append($("<h3></h3>", {
             class: "white"
         })).append($("<textarea></textarea>", {
@@ -47,7 +66,9 @@ function boundary(object, array){
             class: "form-control no-margin",
             placeholder: "<Type...>"
         }).on("keyup", function(){
-            $(".active-party").data("description", $(this).val());
+            var $obj = $("." + $(".active-party")[0].className.split(" ")[1]);
+            $obj.data("description", $(this).val());
+            updateCookieWords();
         }))).on({
             mouseover: function(){
                 enableHide = false;
@@ -130,75 +151,81 @@ function boundary(object, array){
             enableHide = false;
         });
     }
+
+    var $newObject = $("<div></div>", {
+        class: "boundary-layout",
+        id: "boundary-layout-" + parseInt(Math.random() * 100000000000)
+    });
+
+    if (isMultiple){
+        $newObject.addClass("boundary boundary-layout-content");
+        classPrefix = "inner-";
+    }
+
+    if (cookieExist){
+        words = $.parseJSON(getCookie("wrapper-words"))["body"];
+    } else {
+        words = "Jun-07 EUGDRP:::,07 JETBLUE:::,07 JETCOST:::,08 OMANAIR:::,11 ANNOUNCEMENT:::,12 SCTI CLAIM:::,13 DEXE-PHERIN:::,15:::";
+    }
+    var $bodyDiv = $("<div></div>", {
+        class: "boundary " + classPrefix+ "boundary-layout-content"
+    }).appendTo($newObject);
+    words.split(",").forEach(function(word){
+        tmp = word.split(":");
+        $("<p></p>", {
+            "class": "wrapper-body-word " + "word-" + parseInt(Math.random() * 100000),
+            text: tmp[0]
+        }).data({
+            date: tmp[1],
+            importance: tmp[2],
+            description: tmp[3]
+        }).on({
+            mouseover: function(e){
+                enableHide = false;
+                $(".active-party").removeClass("active-party");
+                $(this).addClass("active-party");
+                showTooltip($(this), e.originalEvent);
+            },
+            mouseleave: function(){
+                enableHide = true;
+                setTimeout(hideTooltip, 500);
+            }
+        }).appendTo($bodyDiv);
+    });
+
+    $("<div></div>", {
+        class: "boundary " + classPrefix+ "left-boundary",
+        html: makeTooltipText(array["left"])
+    }).prependTo($newObject);
+
+    $("<div></div>", {
+        class: "boundary " + classPrefix+ "top-boundary",
+        html: makeTooltipText(array["top"])
+    }).prependTo($newObject);
+
+    $("<div></div>", {
+        class: "boundary " + classPrefix+ "right-boundary",
+        html: makeTooltipText(array["right"])
+    }).appendTo($newObject);
+
+    $("<div></div>", {
+        class: "boundary " + classPrefix+ "bottom-boundary",
+        html: makeTooltipText(array["bottom"])
+    }).appendTo($newObject);
+
     $object.each(function(index,obj){
-        $object = $(obj);
-        //if ($object.hasClass("boundary-layout")){
-        //    $object.find(".left-boundary").html(array[0]);
-        //    $object.find(".top-boundary").html(array[1]);
-        //    $object.find(".right-boundary").html(array[2]);
-        //    $object.find(".bottom-boundary").html(array[3]);
-        //    return true;
-        //}
-
-        var $newObject = $("<div></div>", {
-            class: "boundary-layout",
-            id: "boundary-layout-" + parseInt(Math.random() * 100000000000)
-        });
-
+        $obj = $(obj);
         if (isMultiple){
-            $newObject.addClass("boundary boundary-layout-content");
-            classPrefix = "inner-";
-        }
-
-        var words = "<span>Jun-07 EUGDRP</span><br/>" +
-        "<span>07 JETBLUE</span><br/>" +
-        "<span>07 JETCOST</span><br/>" +
-        "<span>08 OMANAIR</span><br/>" +
-        "<span>11 ANNOUNCEMENT</span><br/>" +
-        "<span>12 SCTI CLAIM</span><br/>" +
-        "<span>13 DEXE-PHERIN</span><br/>" +
-        "<span>15</span>";
-        $newObject.html("<div class='boundary " + classPrefix+ "boundary-layout-content'>" + words + "</div>").find("span").each(function(i,el){
-            $(el).on({
-                mouseover: function(e){
-                    enableHide = false;
-                    $(".active-party").removeClass("active-party");
-                    $(this).addClass("active-party");
-                    showTooltip($(this), e.originalEvent);
-                },
-                mouseleave: function(){
-                    enableHide = true;
-                    setTimeout(hideTooltip, 500);
-                }
-            }).attr("id", "word-" + parseInt(Math.random() * 100000));
-        });
-        if (isMultiple){
-            $object.find(".boundary-layout-content").replaceWith($newObject);
-            height = $newObject.innerHeight() - 3 * parseInt($object.css("font-size"));
+            $obj.find(".boundary-layout-content").replaceWith($newObject.clone(true, true));
+            height = $obj.find(".boundary-layout-content").innerHeight() - 3 * parseInt($obj.css("font-size")) - 20;
+            $obj.find("." + classPrefix+ "left-boundary").css("width", height);
+            $obj.find("." + classPrefix+ "right-boundary").css("width", height);
         } else {
-            $newObject.prependTo($object);
-            height = $object.innerHeight() - 3 * parseInt($object.css("font-size")) - 20;
+            $obj.prepend($newObject.clone(true, true));
+            height = $obj.innerHeight() - 3 * parseInt($obj.css("font-size")) - 20;
+            $obj.find("." + classPrefix+ "left-boundary").css("width", height);
+            $obj.find("." + classPrefix+ "right-boundary").css("width", height);
         }
-
-        $("<div></div>", {
-            class: "boundary " + classPrefix+ "left-boundary",
-            html: makeTooltipText(array[0])
-        }).css("width", height).prependTo($newObject);
-
-        $("<div></div>", {
-            class: "boundary " + classPrefix+ "top-boundary",
-            html: makeTooltipText(array[1])
-        }).prependTo($newObject);
-
-        $("<div></div>", {
-            class: "boundary " + classPrefix+ "right-boundary",
-            html: makeTooltipText(array[2])
-        }).css("width", height).appendTo($newObject);
-
-        $("<div></div>", {
-            class: "boundary " + classPrefix+ "bottom-boundary",
-            html: makeTooltipText(array[3])
-        }).appendTo($newObject);
 
     });
 
@@ -206,20 +233,20 @@ function boundary(object, array){
 }
 
 function makeTooltipText(text){
-    var arr = text.split(" "), result = [], length = text.length;
+    var arr = text.split(","), result = [], length = text.length, wordData;
     arr.forEach(function(word){
         if (word != ""){
+            wordData = word.split(":");
             result.push($("<div></div>", {
-                class: "tagger-word",
-                text: word,
-                id: "word-" + parseInt(Math.random() * 100000)
+                class: "tagger-word " + "word-" + parseInt(Math.random() * 100000),
+                text: wordData[0]
             }).css({
                 width: parseInt(word.length / length * 100) + "%",
-                "min-width": word.length * 0.5 + "em"
+                "min-width": word.length * 0.4 + "em"
             }).data({
-                date: "",
-                importance: "",
-                description: ""
+                date: wordData[1],
+                importance: wordData[2],
+                description: wordData[3]
             }).on({
                 mouseover: function(e){
                     enableHide = false;
@@ -247,11 +274,6 @@ function showTooltip($obj, event){
         top = window.innerHeight - $tooltipObj.innerHeight();
     }
     $tooltipObj.find("#event-date").val($obj.data("date"));
-    if ($obj.data("importance") == ""){
-        //$tooltipObj.find("#tagger-message-select").
-    } else {
-        $tooltipObj.find("#tagger-message-select").val($obj.data("importance"));
-    }
     $tooltipObj.find("#tagger-message-select").val($obj.data("importance"));
     $tooltipObj.find("#tooltip-description").val($obj.data("description"));
     $tooltipObj.css({
@@ -276,4 +298,53 @@ function showMessageTooltip(){
         left: left,
         top: top
     }).show();
+}
+
+function updateCookieWords(){
+    var $obj = $($(".boundary-layout")[0]), words = {}, outerWords = {left: "", top: "", right: "", bottom: ""}, innerWords = {left: "", top: "", right: "", bottom: ""}, bodyWords = "", tmp;
+    $obj.find(".left-boundary").find(".tagger-word").each(function(i, el){
+        tmp = $(el).text() + ":" + $(el).data("date") + ":" + $(el).data("importance") + ":" + $(el).data("description") + ",";
+        outerWords.left += tmp;
+    });
+    $obj.find(".top-boundary").find(".tagger-word").each(function(i, el){
+        tmp = $(el).text() + ":" + $(el).data("date") + ":" + $(el).data("importance") + ":" + $(el).data("description") + ",";
+        outerWords.top += tmp;
+    });
+    $obj.find(".right-boundary").find(".tagger-word").each(function(i, el){
+        tmp = $(el).text() + ":" + $(el).data("date") + ":" + $(el).data("importance") + ":" + $(el).data("description") + ",";
+        outerWords.right += tmp;
+    });
+    $obj.find(".bottom-boundary").find(".tagger-word").each(function(i, el){
+        tmp = $(el).text() + ":" + $(el).data("date") + ":" + $(el).data("importance") + ":" + $(el).data("description") + ",";
+        outerWords.bottom += tmp;
+    });
+
+
+    $obj.find(".inner-left-boundary").find(".tagger-word").each(function(i, el){
+        tmp = $(el).text() + ":" + $(el).data("date") + ":" + $(el).data("importance") + ":" + $(el).data("description") + ",";
+        innerWords.left += tmp;
+    });
+    $obj.find(".inner-top-boundary").find(".tagger-word").each(function(i, el){
+        tmp = $(el).text() + ":" + $(el).data("date") + ":" + $(el).data("importance") + ":" + $(el).data("description") + ",";
+        innerWords.top += tmp;
+    });
+    $obj.find(".inner-right-boundary").find(".tagger-word").each(function(i, el){
+        tmp = $(el).text() + ":" + $(el).data("date") + ":" + $(el).data("importance") + ":" + $(el).data("description") + ",";
+        innerWords.right += tmp;
+    });
+    $obj.find(".inner-bottom-boundary").find(".tagger-word").each(function(i, el){
+        tmp = $(el).text() + ":" + $(el).data("date") + ":" + $(el).data("importance") + ":" + $(el).data("description") + ",";
+        innerWords.bottom += tmp;
+    });
+
+    $obj.find(".wrapper-body-word").each(function(i, el){
+        tmp = $(el).text() + ":" + $(el).data("date") + ":" + $(el).data("importance") + ":" + $(el).data("description") + ",";
+        bodyWords += tmp;
+    });
+
+    words.inner = innerWords;
+    words.outer = outerWords;
+    words.body = bodyWords;
+
+    setCookie("wrapper-words", JSON.stringify(words));
 }
